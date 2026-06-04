7e48d218351c3ae5f26a872d13c6ac979632cdbf
Nexus <timotej.petrik1@gmail.com>
2026-06-04 14:39:24 +0200

    Update 03.py
    Should wait a second before removing the number

--- 03.py
+++ 03.py
@@ -9,10 +9,13 @@ root.geometry("300x300")
 label = tkinter.Label(root, text="", font=("Arial", 72))
 label.pack()
 
+def clear_label():
+    label.config(text="")
+
 number = random.randint(10000,100000)
 label.config(text=number)
-time.sleep(1)
-label.config(text="")
+label.after(1000, clear_label)
+
 
 
 
