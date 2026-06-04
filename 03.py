4ba9317fb6a65ec77f076d7be143eb877432b06d
Nexus <timotej.petrik1@gmail.com>
2026-06-04 14:54:43 +0100

    completed

--- 03.py
+++ 03.py
@@ -1,23 +1,39 @@
 import tkinter
 import random
-import time
 
+
+number = random.randint(10000,100000)
+label = None
+label2 = None
+
+def check_answer():
+    answer = input("Ake bolo moje cislo? ")
+    if answer == str(number):
+        label.config(text="SUPER")
+    else:
+        label.config(text="ZLE")
+        label2.config(text=f"Bolo to {number}")
+
+def remove_label():
+    label.config(text="")
+    root.after(100, check_answer)
 
 root = tkinter.Tk()
 root.geometry("300x300")
 
-label = tkinter.Label(root, text="", font=("Arial", 72))
+label = tkinter.Label(root, text="", font=("Arial", 60))
 label.pack()
 
-def clear_label():
-    label.config(text="")
+label2 = tkinter.Label(root, text="", font=("Arial", 24))
+label2.pack()
+
 
 number = random.randint(10000,100000)
 label.config(text=number)
-label.after(1000, clear_label)
 
-
-
+root.after(1000, remove_label)
 
 root.mainloop()
+
+
 
